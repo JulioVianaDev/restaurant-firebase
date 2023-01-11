90bb4d5e536c7bba8939746451dbd7cc9d4cd04b
juliovianadev <juliovianadev@gmail.com>
2023-01-11 13:27:29 -0300

    incrementando as actions do reducer do cart

--- src/Layout/HeaderCartButton.js
+++ src/Layout/HeaderCartButton.js
@@ -1,13 +1,22 @@
 import CartIcon from "../Cart/CartIcon";
 import classes from './HeaderCartButton.module.css'
+
+import { useContext } from "react";
+import CartContext from "../context/cart-context";
+
 const HeaderCartButton = props =>{
+  const cartCtx = useContext(CartContext);
+  
+  const numberOfCartItems = cartCtx.items.reduce((currentNumber,item)=>{
+    return currentNumber + item.amount
+  },0);
   return <button className={classes.button} onClick={props.onClick}>
     <span className={classes.icon}>
       <CartIcon/>
     </span>
     <span>You Cart</span>
     <span className={classes.badge}>
-      3
+      {numberOfCartItems}
     </span>
   </button>
 }
--- src/context/CartProvider.js
+++ src/context/CartProvider.js
@@ -1,14 +1,35 @@
 import CartContext from "./cart-context"
+import { useReducer } from "react"
+
+const defaultState={
+  items: [],
+  totalAmount:0,
+}
+const cartReducer=(state,action)=>{
+  if (action.type === "ADD"){
+    const updatedItems = state.items.concat(action.item)
+    const updatedTotalAmount = state.totalAmount + action.item.price * action.type.amount
+    return{
+      items: updatedItems,
+      totalAmount: updatedTotalAmount
+    }
+  }
+  return  defaultState
+}
 
 const CartProvider = props=>{
+  const [ cartState,dispatchCartAction]=useReducer(cartReducer,defaultState)
+  const addItemToCartHandler = item=>{
+      dispatchCartAction({type: 'ADD', item: item})
+  }
 
-  const addItemToCartHandler = item=>{}
-
-  const removeItemToCartHandler = id =>{}
+  const removeItemToCartHandler = id =>{
+    dispatchCartAction({type: 'REMOVE', id: id})
+  }
 
   const cartContext={
-    items: [],
-    totalAmount:0,
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemToCartHandler,
   }
