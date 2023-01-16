8706645bac9025a8f65e21b4d7623b20d3da8351
juliovianadev <juliovianadev@gmail.com>
2023-01-16 10:31:16 -0300

    update add value of contex

--- src/Meals/Mealitem/MealItem.js
+++ src/Meals/Mealitem/MealItem.js
@@ -1,8 +1,20 @@
 import classes from './MealItem.module.css'
+import { useContext } from 'react';
+import CartContext from '../../context/cart-context';
 import MealItemForm from './MealItemForm';
 const MealItem = props =>{
   
   const price = `R$${props.price.toFixed(2)}`
+  const ctx = useContext(CartContext)
+  const addCartHandler = amount =>{
+    ctx.addItem({
+      id: props.id,
+      name: props.name,
+      amount: amount,
+      price: props.price
+
+    })
+  }
   return <li className={classes.meal}>
     <div>
       <div><h3>{props.name}</h3></div>
@@ -10,7 +22,7 @@ const MealItem = props =>{
       <div className={classes.price}>{price}</div>
     </div>
     <div>
-    <MealItemForm/>
+      <MealItemForm onAddToCart={addCartHandler}/>
     </div>
   </li>
 }
