6d131d369b18de3596e492ad61878a8a2625ca9b
juliovianadev <juliovianadev@gmail.com>
2023-01-16 14:56:22 -0300

    useEffect finalizado para efeito de bump

--- src/Layout/HeaderCartButton.js
+++ src/Layout/HeaderCartButton.js
@@ -1,16 +1,30 @@
 import CartIcon from "../Cart/CartIcon";
 import classes from './HeaderCartButton.module.css'
 
-import { useContext } from "react";
+import { useContext,useEffect, useState } from "react";
 import CartContext from "../context/cart-context";
 
 const HeaderCartButton = props =>{
   const cartCtx = useContext(CartContext);
-  
-  const numberOfCartItems = cartCtx.items.reduce((currentNumber,item)=>{
+  const [btnIsHigh,setBtnIsHigh] = useState(false)
+  const {items } = cartCtx;
+  const numberOfCartItems = items.reduce((currentNumber,item)=>{
     return currentNumber + item.amount
   },0);
-  return <button className={classes.button} onClick={props.onClick}>
+  const btnClasses= `${classes.button} ${ btnIsHigh? classes.bump : ''}`
+  useEffect(()=>{
+    if(items.length ===0){
+      return
+    }
+    setBtnIsHigh(true)
+    const timer = setTimeout(()=>{
+      setBtnIsHigh(false)
+    },300);
+    return()=>{
+      clearTimeout(timer)
+    }
+  },[items])
+  return <button className={btnClasses} onClick={props.onClick}>
     <span className={classes.icon}>
       <CartIcon/>
     </span>
